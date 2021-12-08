7117dd41fa5bc180fce1c922f8d8c87f234142d2
DiegoGDP <Diego.Guerrero@alu.uclm.es>
2021-12-08 13:09:30 +0100

    Iteration4 v0.0.0

---
+++ Iteration4/src/main/java/Presentation/UI_Patient.java
@@ -0,0 +1,17 @@
+package Presentation;
+
+import it2_package.Domain;
+
+public class UI_Patient {
+
+	public String showByState() {
+		// TODO - implement UI_Patient.showByState
+		throw new UnsupportedOperationException();
+	}
+
+	public void showContactNetwork() {
+		// TODO - implement UI_Patient.showContactNetwork
+		throw new UnsupportedOperationException();
+	}
+
+}
