1650c6d9229e5ea8b805221314445bc0c52b9379
RosAlvarez <Rosa.Alvarez@alu.uclm.es>
2021-12-09 12:58:14 +0100

    implementation v0.0.1

---
+++ Iteration2/src/main/java/it2_package/Domain/Infection.java
@@ -0,0 +1,12 @@
+package it2_package.Domain;
+
+import java.util.Date;
+
+public class Infection {
+
+	private boolean worsening;
+	private Date initialDate;
+	private Date endDate;
+	private String severeness;
+
+}
